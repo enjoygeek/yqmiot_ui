6dacb9459ac1f0dded780469dfeb27fabf0e9b14
phunsuke <phunsukewonder@outlook.com>
2017-03-15 21:54:04 +0800

    增加组件List

--- web/src/pages/index.vue
+++ web/src/pages/index.vue
@@ -6,41 +6,14 @@
       </mu-appbar>
 
       <div id="list">
-        <mu-list>
-          <mu-list-item disableRipple title="你的猫" @click="handleToggle('events')">
-            <mu-icon slot="left" value="send"/>
-            <sub diplay="inline">觅食中</sub>
-            <mu-switch v-model="events"  slot="right"/>
-          </mu-list-item>
-          <mu-list-item title="Drafts">
-            <mu-icon slot="left" value="drafts"/>
-          </mu-list-item>
-          <mu-list-item title="Inbox" toggleNested>
-            <mu-icon slot="left" value="inbox"/>
-            <mu-list-item slot="nested" title="Starred">
-              <mu-icon slot="left" value="grade"/>
-            </mu-list-item>
-            <mu-list-item slot="nested" disabled title="Sent mail" toggleNested>
-              <mu-icon slot="left" value="send"/>
-              <mu-list-item title="Drafts" slot="nested">
-                <mu-icon slot="left" value="drafts"/>
-              </mu-list-item>
-            </mu-list-item>
-            <mu-list-item slot="nested" title="Inbox" toggleNested>
-              <mu-icon slot="left" value="inbox"/>
-              <mu-list-item title="Drafts" slot="nested">
-                <mu-icon slot="left" value="drafts"/>
-              </mu-list-item>
-            </mu-list-item>
-          </mu-list-item>
-        </mu-list>
+        <router-view></router-view>
       </div>
-      
+
       <mu-bottom-nav :value="bottomNav" shift @change="handleChange" id="footer">
-        <mu-bottom-nav-item value="movies" title="Movies" icon="ondemand_video"/>
-        <mu-bottom-nav-item value="music" title="Music" icon="music_note"/>
-        <mu-bottom-nav-item value="books" title="Books" icon="books"/>
-        <mu-bottom-nav-item value="pictures" title="Pictures" icon="photo"/>
+        <mu-bottom-nav-item value="list" title="设备" icon="ondemand_video"/>
+        <mu-bottom-nav-item value="music" title="信息" icon="music_note"/>
+        <mu-bottom-nav-item value="books" title="添加" icon="books"/>
+        <mu-bottom-nav-item value="pictures" title="我的" icon="photo"/>
       </mu-bottom-nav>
     </div>
   </div>
@@ -51,22 +24,17 @@ export default {
   name: 'index',
   data () {
     return {
-      msg: 'Welcome to 月球猫互联',
       myron,
-      bottomNav: 'movies',
-      bottomNavColor: 'movies',
+      bottomNav: 'list',
       events: false,
     }
   },
   methods: {
     handleChange (val) {
-      this.bottomNav = val
+      this.bottomNav = val;
+      this.$router.push(`/${val}`);
     },
-    handleToggle (key) {
-      this[key] = !this[key];
-      // this.events = !this.events;
-    }
-  }
+  },
 }
 </script>
 
@@ -86,19 +54,29 @@ export default {
     left: 0px;
     right: 0px;
     background-color: rgba(0,0,0,0.25);
+      #shadow{
+        position:absolute;
+        top:0px;
+        left:0px;
+        height:8%;
+        box-sizing:border-box;
+      }
       #list{
         position:absolute;
-        top:6.8%;
-        bottom:6%;
+        top:8%;
+        bottom:8%;
         left:0px;
         right:0px;
         overflow-y:scroll;
         -webkit-overflow-scrolling:touch;
+        box-sizing:border-box;
       }
       #footer{
       position:absolute;
       bottom: 0px;
       left:0px;
+      height:8%;
+      box-sizing:border-box;
     }
   }
 }
