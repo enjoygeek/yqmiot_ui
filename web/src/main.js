09396cb57283b7e9baa6e098d25bfeca9472c769
phunsuke <phunsukewonder@outlook.com>
2017-03-15 17:04:22 +0800

    增加ui支持

--- web/src/main.js
+++ web/src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import './assets/theme.less'
 Vue.config.productionTip = false
-
+Vue.use(MuseUI);
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
