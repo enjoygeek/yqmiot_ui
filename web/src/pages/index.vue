a118b35312f0a0001df1b52f4d729711030c901e
phunsuke <phunsukewonder@outlook.com>
2017-03-15 17:04:51 +0800

    增加登录界面

---
+++ web/src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to 月球猫互联'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ web/src/pages/login.vue
@@ -0,0 +1,62 @@
+<template>
+ <div class="wrap">
+     <div class="login">
+        <div class="marg">{{msg}}</div>
+        <img src="../assets/yqm2.png" alt="">
+        <div class="marg"></div>
+        <mu-auto-complete hintText="请随便输入用户名" :dataSource="dataSource" style="color:#fff" />
+        <mu-auto-complete hintText="请随便输入密码" :dataSource="dataSource" style="color:#fff" />
+         <mu-raised-button label="login" class="demo-raised-button" id="buton" secondary/>
+    </div>
+ </div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: '月球猫互联登录界面',
+      dataSource: [],
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+ .wrap{
+     position: absolute;
+     top: 0px;
+     bottom: 0px;
+     left: 0px;
+     right: 0px;
+     background: #545454;
+     .login{
+        position: absolute;
+        top: 0px;
+        bottom: 0px;
+        left: 0px;
+        right: 0px;
+        background-color: rgba(0,0,0,0.25);
+        text-align: center;
+        .marg{
+            margin-top:25%;
+            margin-bottom:10%;
+            color:#fff;
+            font-size: 1.5em;
+            font-weight: 800;
+            text-shadow: 1px 4px 0px #666;
+        }
+        .label{
+            font-size: 1.5em;
+            color:#fff;
+            text-align:left;
+            text-indent:3em;
+        }
+        #buton{
+            background-color: rgba(0,0,0,0.5);
+            width:70%;
+        }
+    }
+ }
+</style>
