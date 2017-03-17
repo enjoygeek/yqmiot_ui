808a4b42cc32164372af50897cd63c6de75e5950
phunsuke <phunsukewonder@outlook.com>
2017-03-17 15:15:47 +0800

    增加测试代码

--- web/server/index.js
+++ web/server/index.js
@@ -2,7 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mqtt = require('mqtt');
-
+var yqm = mqtt.connect('ws://test.mosquitto.org:8080');
+yqm.on('connect',function(){
+	yqm.publish('yqmiot/2/2/5/ping');
+});
 app.get('/', function(req, res){
 	res.send('<h1>Welcome Realtime Server</h1>');
 });
