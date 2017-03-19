f2bfe3ec41ec8f395c98f1c21dc982ffa0bf89fa
phunsuke <phunsukewonder@outlook.com>
2017-03-19 09:17:57 +0800

    增加超时处理

--- web/src/store/modules/homeSwitch.js
+++ web/src/store/modules/homeSwitch.js
@@ -1,7 +1,7 @@
 import * as types from '../mutation-types'
 import IOT from '../../api/client'
 const state = {
-    homeSwitch:[
+    homeSwitch: [
         {
             name: "月球猫开关",
             receiver: "switch0",
@@ -10,26 +10,31 @@ const state = {
         {
             name: "kira开关",
             receiver: "switch1",
-            val:true,
+            val: true,
         }
     ],
     timeout: false,
+    timeout1: true,
 }
 
 const getters = {
 
 }
-
 const actions = {
     toggle({ commit }, message) {
-        console.log("publish toggle");
-        IOT.socket.emit("publish", { topic: `yqmiot/${IOT.account}/${message.receiver}/${IOT.nodeId}/call`, message: { "action": "yqmiot.event.toggle", "sender": IOT.nodeId, "name": message.name, "receiver": message.receiver, "value": message.val } });
+        IOT.sendCallToggle(IOT,message);
     }
 }
 const mutations = {
-    // homeSwitch({ commit }, payload) {
-    //     state.message = payload;
-    // }
+    homeSwitch({ commit }, payload) {
+        IOT.flag = true;
+    },
+    timeout({commit},that){
+        that.status = !that.status;
+        that.toast = true
+        if (that.toastTimer) clearTimeout(that.toastTimer)
+        that.toastTimer = setTimeout(() => { that.toast = false }, 2000)       
+    }
 }
 
 export default {
