eb5804d900c72fe52c909920a7ac841726934c4a
phunsuke <phunsukewonder@outlook.com>
2017-03-19 17:17:29 +0800

    更改客户端平台

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -8,10 +8,10 @@ import call from './modules/call'
 import ack from './modules/ack'
 import createWebSocketPlugin from './plugin'
 VUE.use(Vuex);
-IOT.init(2, 5);
+IOT.init(1, 5);
 IOT.socket.on("connect",function(){
     IOT.socket.emit('subscribe',{topic:"yqmiot/#"});
-    IOT.socket.emit("publish",{topic:"yqmiot/"+IOT.account+"/2/"+IOT.nodeId+"/call",message:{"name":"property"}})
+    IOT.socket.emit("publish",{topic:"yqmiot/"+IOT.account+"/1000/"+IOT.nodeId+"/call",message:{action:"yqmiot.call.property",sender:IOT.nodeId,receiver:'1000',name:"event"}})
 });
 
 const plugin = createWebSocketPlugin(IOT.socket);
