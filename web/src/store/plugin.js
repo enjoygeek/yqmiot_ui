c328ab6b32822ca16a98b1a2b5952d8feea63201
phunsuke <phunsukewonder@outlook.com>
2017-03-19 09:17:48 +0800

    消息处理插件

--- web/src/store/plugin.js
+++ web/src/store/plugin.js
@@ -4,6 +4,7 @@ export default function createWebSocketPlugin(socket) {
         socket.on('mqtt', data => {
               store.commit('receiveData', data)
             let mes = data.topic.split("/");
+            console.log(mes)
             if (mes[3] != IOT.nodeId)
                 store.commit(mes[4], data)            
         })      
