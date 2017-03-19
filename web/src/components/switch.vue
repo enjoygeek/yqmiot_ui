eeb971b1aa760471a037454ae870c7aa6dd5ee5b
phunsuke <phunsukewonder@outlook.com>
2017-03-19 09:16:55 +0800

    增加switch.vue组件

---
+++ web/src/components/switch.vue
@@ -0,0 +1,127 @@
+<template>
+  <label
+    :data-disabled="disabled"
+    :data-theme="theme"
+    :style="{
+      'font-size':size
+    }"
+    :class="['vue-ios7-switch',cssClass]">
+    <input type="checkbox"
+    :checked="checked"
+    :disabled="disabled"
+    >
+    <span></span>
+  </label>
+</template>
+
+<script>
+  export default {
+    name: 'switch',
+    props:{
+      cssClass:String,
+      checked:Boolean,
+      disabled:Boolean,
+      size:String,
+      theme:{
+        type:String,
+        default:'green'
+      }
+    }
+  };
+</script>
+
+<style lang="less">
+@vue-ios7-switch-blue:#26a2ff;
+@vue-ios7-switch-green:#4cd964;
+.vue-ios7-switch {
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+  tap-highlight-color: transparent;
+
+  input {
+    opacity: 0;
+    position: absolute;
+
+    &+span {
+      position: relative;
+      display: inline-block;
+      width: 1.65em;
+      height: 1em;
+      background: white;
+      box-shadow: inset 0 0 0 0.0625em #e9e9e9;
+      border-radius: 0.5em;
+      vertical-align: -0.15em;
+      transition: all 0.40s cubic-bezier(.17,.67,.43,.98);
+    }
+  }
+
+  &:active + span,
+  input + span:active {
+    box-shadow: inset 0 0 0 0.73em #e9e9e9;
+  }
+
+  input + span:after {
+    position: absolute;
+    display: block;
+    content: '';
+    width: 0.875em;
+    height: 0.875em;
+    border-radius: 0.4375em;
+    top: 0.0625em;
+    left: 0.0625em;
+    background: white;
+    box-shadow: inset 0 0 0 0.03em rgba(0,0,0,0.1), 0 0 0.05em rgba(0,0,0,0.05), 0 0.1em 0.2em rgba(0,0,0,0.2);
+    transition: all 0.25s ease-out;
+  }
+
+  &:active input + span:after,
+  input + span:active:after {
+    width: 1.15em;
+  }
+
+  input:checked + span:after {
+    left: 0.7125em;
+  }
+
+  &:active input:checked + span:after,
+  input:checked + span:active:after {
+    left: 0.4375em;
+  }
+
+  input:focus + span:after {
+    box-shadow: inset 0 0 0 0.03em rgba(0,0,0,0.15), 0 0 0.05em rgba(0,0,0,0.08), 0 0.1em 0.2em rgba(0,0,0,0.3);
+    background: #fff;
+  }
+
+  input:focus + span {
+    box-shadow: inset 0 0 0 0.0625em #dadada;
+  }
+
+  &[data-disabled]{
+    pointer-events: none;
+    opacity: 0.6;
+  }
+  &[data-theme="green"]{
+    input:checked + span {
+      box-shadow: inset 0 0 0 0.73em @vue-ios7-switch-green;
+    }
+
+    input:focus:checked + span {
+      box-shadow: inset 0 0 0 0.73em darken(@vue-ios7-switch-green,10%);
+    }
+  }
+
+  &[data-theme="blue"]{
+    input:checked + span {
+      box-shadow: inset 0 0 0 0.73em @vue-ios7-switch-blue;
+    }
+    input:focus:checked + span {
+      box-shadow: inset 0 0 0 0.73em darken(@vue-ios7-switch-blue,10%);
+    }
+  }
+
+}
+</style>
