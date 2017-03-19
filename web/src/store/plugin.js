6459c8c03182cfd790f782c316291119b5e0e5d6
phunsuke <phunsukewonder@outlook.com>
2017-03-19 17:17:51 +0800

    完善socket插件

--- web/src/store/plugin.js
+++ web/src/store/plugin.js
@@ -2,11 +2,13 @@ import IOT from '../api/client'
 export default function createWebSocketPlugin(socket) {
     return store => {
         socket.on('mqtt', data => {
-              store.commit('receiveData', data)
+            //   store.commit('receiveData', data)
             let mes = data.topic.split("/");
-            console.log(mes)
-            if (mes[3] != IOT.nodeId)
-                store.commit(mes[4], data)            
+                store.commit(mes[4], data)
+            if(data){
+                //回包
+                // socket.emit("")
+            }          
         })      
     }
 }
