52d8bb8c884ee55bbbb180451b05501393f913d3
phunsuke <phunsukewonder@outlook.com>
2017-03-21 16:58:45 +0800

    增加property上报处理

--- web/src/store/modules/property.js
+++ web/src/store/modules/property.js
@@ -1,5 +1,6 @@
 import * as types from '../mutation-types'
 import IOT from '../../api/client'
+import device from './device'
 const state = {
     collection: {},
 }
@@ -12,8 +13,21 @@ const actions = {
 
 }
 const mutations = {
-    property({ commit }, payload) {
-        console.log(payload);
+    property({ commit }, message) {
+        let types = IOT.decodeEvent(message);
+        let payload = JSON.parse(message.payload);
+        let device = IOT.updateProperty(types.sender, "homeswitch");
+        if (device) {
+            device.val = payload.params.id;
+        } else {
+            var newDevice = {
+                name: payload.params.name,
+                id: types.sender,
+                val: false,
+                avatar: "",
+            }
+            IOT.device.homeswitch.push(newDevice);
+        }
     }
 }
 
