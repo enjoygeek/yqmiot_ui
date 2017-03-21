ce36ed7670a8aefa7755098e078065a31e06d168
phunsuke <phunsukewonder@outlook.com>
2017-03-21 16:58:27 +0800

    废弃homeSwitch.js的使用

--- web/src/store/modules/homeSwitch.js
+++ web/src/store/modules/homeSwitch.js
@@ -4,13 +4,13 @@ const state = {
     homeSwitch: [
         {
             name: "月球猫开关",
-            id: "1000",
+            id: "3000",
             val: false,
             avatar: "",
         },
         {
             name: "kira开关",
-            id: "switch1",
+            id: "2000",
             val: true,
             avatar: "",
         }
@@ -21,20 +21,10 @@ const getters = {
 
 }
 const actions = {
-    [cmd.YQMIOT_EVNET_TOGGLE]({ commit }, message) {
-        IOT.sendCall(message);
-    }
+    
 }
 const mutations = {
-    homeSwitch({ commit }, payload) {
-        // IOT.flag = true;
-    },
-    timeout({ commit }) {
-        IOT.el.status = !IOT.el.status;//超时，状态不改变，返回原状态。
-        IOT.toast("请求超时");//通知用户已经超时
-        clearTimeout(IOT.outTimer);//清除定时器。
-        IOT.el.disabled = false; //解除不可操作。使用户可以再次发送。
-    }
+    
 }
 
 export default {
